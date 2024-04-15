fc8fdb7cc196c5247b4eb0ae2c27d89582fec119
brandy4345 <brandy4345@gmail.com>
2024-04-15 14:43:50 -0400

    Se avanzo con las validaciones

--- JavaScript/agregarPedido/agregar-pedido.js
+++ JavaScript/agregarPedido/agregar-pedido.js
@@ -1,38 +1,143 @@
+// ----VARIABLES Y CONSTANTES-----
+const limit=5;
+
+
 // ------ FUNCIONES ---------
 const isChecked = () => { 
     if (radio_fruta.checked){
-        producto_fruta.style.display="flex";
-        producto_verdura.style.display="none";
+        esconder_fruta.style.display="block";
+        esconder_error_verdura.style.display="none"
+        esconder_verdura.style.display="none";
     }
     else if (radio_verdura.checked){
-        producto_fruta.style.display="none";
-        producto_verdura.style.display="flex";
-        console.log("verdura esta checked");
+        esconder_fruta.style.display="none";
+        esconder_error_fruta.style.display="none"
+        esconder_verdura.style.display="block";
     }
 }
 
+const validateRadioButton= (valor) => {
+    if (!valor){
+        return false
+    }
+    else{
+        return true
+    }
+}
+const validateCheckbox = (valor) => {
+    if(valor){
+        if (valor.value=="FRUTA"){
+            let num = 0;
+            for(let i = 0; i<producto_fruta.length;i++){
+                if(producto_fruta[i].checked){
+                    num++;
+                }
+            }
+            if(num>=1 && num<=limit){
+                return true
+            }
+            return false
+        }
+        else if (valor.value=="VERDURA"){
+            let num = 0;
+            for(let i = 0; i<producto_verdura.length;i++){
+                if(producto_verdura[i].checked){
+                    num++;
+                }
+            }
+            if(num>=1 && num<=limit){
+                return true
+            }
+            return false
+        }       
+    }  
+    else {
+        return false;
+    } 
+}
+const validateFotos = (fotos) => {
+    if(fotos.length>=1 && fotos.length<=3){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 const validateLenght = (descripcion) => descripcion && descripcion.length>=3 && descripcion.length<=300;
 
 const validateEmail = (email) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   };
+const validateTelefono = (numero) => {
+    const telefonoRegex = /^(\+?56)?(\s?)(0?9)(\s?)[98765432]\d{7}$/;
+    return telefonoRegex.test(numero);  
+}
+
 
 const validate = () => {
+    // los errores mostrados en html 
+    const error_tipo = document.getElementById("error-tipo");
+    const error_email = document.getElementById("error-email");
+    const error_nombre = document.getElementById("error-nombre");
+    const error_checkbox_fruta = document.getElementById("error-checkbox-fruta");
+    const error_checkbox_verdura = document.getElementById("error-checkbox-verdura");
+    const error_fotos = document.getElementById("error-foto");
+    const error_telefono = document.getElementById("error-telefono");
+
     // Recuperar lo que va a ser testeado :)
+    const valor = document.querySelector('input[name="Tipo-fruta-o-verdura"]:checked');
     const descTextArea = document.getElementById("desc-producto");
+    const fotos = document.getElementById("foto").files;
     const nombreProductor = document.getElementById("nombre");
     const email  = document.getElementById("user-email"); 
+    const telefono = document.getElementById("telefono");
 
+    const radioISValid = validateRadioButton(valor);
+    const checkboxIsValid = validateCheckbox(valor);
+    const fotosIsValid = validateFotos(fotos);
     const nombreIsValid = validateLenght(nombreProductor.value);
     const emailIsValid = validateEmail(email.value);
+    const telefonoIsValid = validateTelefono(telefono.value)
+    if (!radioISValid){     
+        error_tipo.style.display="block";
+    }
+    else{
+        error_tipo.style.display="none";
+    }
+    if (radioISValid && !checkboxIsValid){
+        console.log(valor.value);
+        if(valor.value == "FRUTA"){
+            error_checkbox_fruta.style.display="block";
+        }
+        if(valor.value == "VERDURA"){
+            error_checkbox_verdura.style.display="block";
+        }
+    } else {
+        error_checkbox_fruta.style.display="none";
+        error_checkbox_verdura.style.display="none";
+    }
+    if (!fotosIsValid){
+        error_fotos.style.display="block";
+    }else{
+        error_fotos.style.display="none";
+    }
+    if (!nombreIsValid){
+        error_nombre.style.display="block";
+    } else{
+        error_nombre.style.display="none";
+    }
+    if(!emailIsValid){
+        error_email.style.display= "block";
+    } else{
+        error_email.style.display="none";
+    }
+    if(!telefonoIsValid){
+        error_telefono.style.display ="block";
+    } else {
+        error_telefono.style.display = "none";
+    }
 
-    if (emailIsValid){
-        console.log("hola");
-    }
-    else {
-        console.log("elpepe");
-    }
 }
 
 
@@ -42,11 +147,17 @@ const validate = () => {
 const radio_fruta = document.getElementById("fruta");
 const radio_verdura = document.getElementById("verdura");
 const fruta_o_verdura = document.querySelector("#Tipo-fruta-o-verdura");
+const esconder_fruta = document.getElementById("esconder-fruta")
+const esconder_verdura = document.getElementById("esconder-verdura")
+const esconder_error_fruta=  document.getElementById("error-checkbox-fruta");
+const esconder_error_verdura=  document.getElementById("error-checkbox-verdura");
+//---checkboxex---
+const producto_fruta = document.getElementById("checkbox-producto-fruta").getElementsByTagName("input");
+const producto_verdura = document.getElementById("checkbox-producto-verdura").getElementsByTagName("input");
 
-const producto_fruta = document.getElementById("checkbox-producto-fruta");
-const producto_verdura = document.getElementById("checkbox-producto-verdura");
 
-const button = document.getElementById("prueba-button");
+
+const button = document.getElementById("submit-button");
 
 //------ Esconde los productos -------
 //producto_fruta.style.display="none";
@@ -58,3 +169,30 @@ fruta_o_verdura.addEventListener("change",isChecked);
 
 button.addEventListener("click",validate);
 
+
+// ----- LIMITAR CHECKBOXES --------
+
+
+
+for (let i = 0; i < producto_fruta.length; i++) {
+    producto_fruta[i].onclick = function() {
+        let checkedcount = 0;
+            for (let i = 0; i < producto_fruta.length; i++) {
+            checkedcount += (producto_fruta[i].checked) ? 1 : 0;
+        }
+        if (checkedcount > limit) {
+            this.checked = false;
+        }
+    }
+}
+for (let i = 0; i < producto_verdura.length; i++) {
+    producto_verdura[i].onclick = function() {
+        let checkedcount = 0;
+            for (let i = 0; i < producto_verdura.length; i++) {
+            checkedcount += (producto_verdura[i].checked) ? 1 : 0;
+        }
+        if (checkedcount > limit) {
+            this.checked = false;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Tarea1web
+
+* En agregar producto para las imágenes se decidió hacer un un input tipo file y que tiene propiedad de multiple, solo en esta tarea se va a usar esa propiedad, ya que para la tarea 2 donde va a existir backend, se va a cambiar de la manera que usa ucursos para recibir archivos.
+
+
+* Agradecimientos a: 
+https://gist.github.com/juanbrujo/0fd2f4d126b3ce5a95a7dd1f28b3d8dd por el json de regiones y comunas
+https://gist.github.com/jaimeguaman/5819511 por el regex del numero de telefono
+https://www.flaticon.es/iconos-gratis/ por los iconos gratis
