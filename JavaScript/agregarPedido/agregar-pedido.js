0a966eb29cfd74fdbe7490a437f8a87c72114bcf
brandy4345 <brandy4345@gmail.com>
2024-04-15 23:06:32 -0400

    Se termino la tarea

--- JavaScript/agregarPedido/agregar-pedido.js
+++ JavaScript/agregarPedido/agregar-pedido.js
@@ -66,6 +66,14 @@ const validateFotos = (fotos) => {
         return false;
     }
 }
+const validateRegionyComuna = (region, comuna) =>  {
+    if( region.value!="" && comuna.value!=""){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 const validateLenght = (descripcion) => descripcion && descripcion.length>=3 && descripcion.length<=80;
 
 const validateEmail = (email) => {
@@ -88,11 +96,14 @@ const validate = () => {
     const error_fotos = document.getElementById("error-foto");
     const error_telefono = document.getElementById("error-telefono");
     const error_descripcion = document.getElementById("error-descripcion");
+    const error_regioncomuna = document.getElementById("error-region");
 
     // Recuperar lo que va a ser testeado :)
     const valor = document.querySelector('input[name="Tipo-fruta-o-verdura"]:checked');
     const descTextArea = document.getElementById("desc-producto");
     const fotos = document.getElementById("foto").files;
+    const region = document.getElementById("regiones");
+    const comuna = document.getElementById("comunas");
     const nombreProductor = document.getElementById("nombre");
     const email  = document.getElementById("user-email"); 
     const telefono = document.getElementById("telefono");
@@ -101,6 +112,7 @@ const validate = () => {
     const checkboxIsValid = validateCheckbox(valor);
     const descIsValid = validateDescripcion(descTextArea);
     const fotosIsValid = validateFotos(fotos);
+    const regionComunaisValid = validateRegionyComuna(region,comuna);
     const nombreIsValid = validateLenght(nombreProductor.value);
     const emailIsValid = validateEmail(email.value);
     const telefonoIsValid = validateTelefono(telefono.value)
@@ -132,6 +144,12 @@ const validate = () => {
     }else{
         error_fotos.style.display="none";
     }
+
+    if (!regionComunaisValid){
+        error_regioncomuna.style.display ="block";
+    } else {
+        error_regioncomuna.style.display ="none";
+    }
     if (!nombreIsValid){
         error_nombre.style.display="block";
     } else{
@@ -147,7 +165,8 @@ const validate = () => {
     } else {
         error_telefono.style.display = "none";
     }
-
+    return radioISValid && checkboxIsValid && descIsValid && fotosIsValid && regionComunaisValid &&
+    nombreIsValid && emailIsValid && telefonoIsValid;
 }
 
 
@@ -161,24 +180,41 @@ const esconder_fruta = document.getElementById("esconder-fruta")
 const esconder_verdura = document.getElementById("esconder-verdura")
 const esconder_error_fruta=  document.getElementById("error-checkbox-fruta");
 const esconder_error_verdura=  document.getElementById("error-checkbox-verdura");
+const modal1 = document.getElementById("myModal");
+const modal2 = document.getElementById("myModal2");
 //---checkboxex---
 const producto_fruta = document.getElementById("checkbox-producto-fruta").getElementsByTagName("input");
 const producto_verdura = document.getElementById("checkbox-producto-verdura").getElementsByTagName("input");
 
 
 
-const button = document.getElementById("submit-button");
-
-//------ Esconde los productos -------
-//producto_fruta.style.display="none";
-//producto_verdura.style.display="none";
-
+const buttonSubmit = document.getElementById("submit-button");
+const btn = document.getElementById("myBtn");
+const btnclose = document.getElementById("cancel-button");
 
 // ------- EVENTOS ----------
 fruta_o_verdura.addEventListener("change",isChecked);
 
-button.addEventListener("click",validate);
-
+btn.onclick = function() {
+    const verificacion = validate();
+    if (verificacion){
+        modal1.style.display = "block";
+    }
+}
+btnclose.onclick = function() {
+    modal1.style.display="none";
+}
+buttonSubmit.onclick = function() {
+    const verificacion2 = validate();
+    //segunda verificacion por si modifican la primera
+    if (verificacion2){
+        modal1.style.display="none";
+        modal2.style.display="block"
+    } else {
+        modal1.style.display = "none"
+    }
+    
+}
 
 // ----- LIMITAR CHECKBOXES --------
 
@@ -206,3 +242,5 @@ for (let i = 0; i < producto_verdura.length; i++) {
         }
     }
 }
+
+
